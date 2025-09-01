7d786cce73c1b4728906e65ef4eb5cb2af38f35a
Marc Caramoan <mcar4177@uni.sydney.edu.au>
2025-09-01 23:20:56 +1000

    #20 - small changes to certificates

--- backend/apps/certificates/models.py
+++ backend/apps/certificates/models.py
@@ -3,20 +3,18 @@
 from django.db import models
 
 class CertificateType(models.Model):
-    certificate_type_id = models.IntegerField(primary_key=True)
     certificate_type = models.CharField(unique=True, max_length=255)
     requires_number = models.BooleanField()
     requires_expiry = models.BooleanField()
 
     class Meta:
-        # managed = False
-        db_table = 'certificate_type'
+        verbose_name = "Certificate Type"
+        # add indexes and constaints
 
 
 class MentorCertificate(models.Model):
     certificate_id = models.IntegerField(primary_key=True)
     certificate_type = models.ForeignKey(CertificateType, models.DO_NOTHING)
-    # Field name made lowercase. Field renamed to remove unsuitable characters. Field renamed because it ended with '_'.
     user_id_fk_field = models.ForeignKey(
         'MentorProfile', models.DO_NOTHING, db_column='user_id (FK)')
     certificate_number = models.CharField(
@@ -28,5 +26,5 @@ class MentorCertificate(models.Model):
     verified = models.BooleanField()
 
     class Meta:
-        # managed = False
-        db_table = 'mentor_certificate'
+        verbose_name = "Mentor Certificate"
+        # add indexes and constaints
