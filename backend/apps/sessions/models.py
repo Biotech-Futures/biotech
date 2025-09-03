2f45d1168e36357e666cb72d5785838cd0765731
Marc Caramoan <mcar4177@uni.sydney.edu.au>
2025-09-03 20:51:19 +1000

    #20 - sanitised Sessions Alerts models

--- backend/apps/sessions/models.py
+++ backend/apps/sessions/models.py
@@ -1,22 +1,54 @@
 # SESSIONS MODELS
 
 from django.db import models
+from django.utils import timezone
+from django.db.models import Q
 
 class Sessions(models.Model):
-    session_id = models.IntegerField(primary_key=True)
-    user = models.ForeignKey('Users', models.DO_NOTHING)
-    access_datetime = models.DateTimeField()
+    user = models.ForeignKey('users.Users', on_delete=models.DO_NOTHING)
+    access_datetime = models.DateTimeField(default=timezone.now)
     isloggedin = models.BooleanField(db_column='isLoggedin')  # Field name made lowercase.
 
     class Meta:
         db_table = 'sessions'
+        verbose_name = "Sessions"
+        ordering = ["-access_datetime"]
+        indexes = [
+            models.Index(fields=['user']),
+            models.Index(fields=['access_datetime'])
+        ]
+        constraints = [
+            # no same sessions
+            models.UniqueConstraint(fields=["user", "access_datetime"], name="unique_user_session"),
+            # check a session is not in future
+            models.CheckConstraint(condition=Q(access_datetime__lte=timezone.now()),
+                                   name="access_not_in_future"),
+            # maybe have a constraint that ensures only one active session? eg check isLoggedIn
+        ]
 
 class Alerts(models.Model):
-    alert_id = models.BigIntegerField(primary_key=True)
-    session = models.ForeignKey('Sessions', models.DO_NOTHING)
-    alert_timestamp = models.DateTimeField()
+    session = models.ForeignKey('Sessions', on_delete=models.CASCADE)
+    alert_timestamp = models.DateTimeField(default=timezone.now)
     error_reason = models.CharField(max_length=255)
-    resolved = models.BooleanField()
+    resolved = models.BooleanField(default=False)
 
     class Meta:
         db_table = 'alerts'
+        verbose_name = "Alerts"
+        ordering = ["-alert_timestamp"]
+        indexes = [
+            models.Index(fields=['session']),
+            models.Index(fields=['resolved'])
+        ]
+        constraints = [
+            # no exact duplicates of alerts
+            models.UniqueConstraint(
+                fields=['session', 'alert_timestamp', 'error_reason'],
+                name='unique_alert_user_session'
+            ),
+            # no empty reason for alert
+            models.CheckConstraint(
+                check=~Q(error_reason=''),
+                name='alert_reason_not_empty'
+            ),
+        ]
