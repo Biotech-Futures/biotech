ec6505715b04ba3da4e5006ace0dc45881c02d0c
Marc Caramoan <mcar4177@uni.sydney.edu.au>
2025-09-06 13:33:47 +1000

    #20 - update to timezone.now for creation datetime, symmetric deleted flag, ensure spaces not allowed

--- backend/apps/groups/models.py
+++ backend/apps/groups/models.py
@@ -2,12 +2,14 @@
 
 from django.db import models
 from django.db.models import Q, F
+from django.db.models.functions import Trim, Length
+from django.utils import timezone
 
 class Groups(models.Model):
     group_name = models.CharField(max_length=255)
     track = models.ForeignKey('Tracks', on_delete=models.PROTECT) # Protect to prevent deletion when referenced track is gone 
     # I thought this might be good just in case tracks are deleted but groups should persist in the instance tracks are moved or removed
-    creation_datetime = models.DateTimeField(default=models.functions.Now) # Default to current time on creation
+    creation_datetime = models.DateTimeField(default=timezone.now) # Default to current time on creation
     deleted_flag = models.BooleanField(default=False) # Default to False for better data integrity
     deleted_datetime = models.DateTimeField(null=True, blank=True) # Allow null/blank for groups that aren't deleted
 
@@ -22,8 +24,11 @@ class Groups(models.Model):
         constraints = [
             # Ensure deleted_flag is True if deleted_datetime is set
             models.CheckConstraint(
-                condition=Q(deleted_datetime__isnull=True) | Q(deleted_flag=True),
-                name='deleted_flag_true_if_deleted_datetime'
+                condition=(
+                    (Q(deleted_flag=True)  & Q(deleted_datetime__isnull=False)) |
+                    (Q(deleted_flag=False) & Q(deleted_datetime__isnull=True))
+                ),
+                name='group_deleted_flag_datetime_consistent',
             ),
             # Ensure group names are unique within the same track
             models.UniqueConstraint(
@@ -37,7 +42,7 @@ class Groups(models.Model):
             ),
             # Ensure group_name is not empty
             models.CheckConstraint(
-                check=~Q(group_name=''),
+                condition=Length(Trim('group_name')) > 0,
                 name='group_name_not_empty'
             ),
             # Ensure creation_datetime is not in the future
