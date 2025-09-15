be454248f41d2888aaf6816ea3028f8eb109a92a
Tyler Signo <tsit2615@uni.sydney.edu.au>
2025-09-16 00:07:45 +1000

    #80 registered endpoint into urls

--- backend/apps/groups/urls.py
+++ backend/apps/groups/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import CountryViewSet, GroupMemberViewSet
+from .views import CountryViewSet, GroupMemberViewSet, TrackViewSet
 
 router = DefaultRouter()
 router.register(r'countries', CountryViewSet, basename="countries")
 router.register(r'group-members', GroupMemberViewSet, basename='group-members')
+router.register(r'tracks', TrackViewSet, basename='tracks')
 urlpatterns = router.urls
