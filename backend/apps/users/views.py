422e1962c930b5c44f244fd27457ce0e0e619ada
Minh <qngu0097@uni.sydney.edu.au>
2025-09-15 14:02:16 +1000

    implemented searching for a paginated, fileterable group of users for issue 42

--- backend/apps/users/views.py
+++ backend/apps/users/views.py
@@ -20,3 +20,29 @@ class UsersRetrieveView(generics.RetrieveAPIView):
     # renderer_classes = [JSONRenderer]
     renderer_classes = [TemplateHTMLRenderer]
     template_name = "users/details.html"
+
+class UserPagePagination(PageNumberPagination):
+    page_size = 10
+    page_query_param = "page"
+    page_size_query_param = "page_size"
+    max_page_size = 100
+
+#Issue 42
+class UserListHTMLView(generics.ListAPIView):
+    # queryset = Users.objects.select_related("track", "state")
+    serializer_class = UserSerializer
+    permission_classes = [permissions.AllowAny]
+
+    pagination_class = UserPagePagination
+    renderer_classes = [TemplateHTMLRenderer]
+    template_name = "users/list.html"
+
+    def get_queryset(self):
+        queryset = User.objects.all()
+        status_param = self.request.query_params.get("status")
+        if status_param is not None:
+            queryset = queryset.filter(status=status_param)
+        email_param = self.request.query_params.get("email")
+        if email_param is not None:
+            queryset = queryset.filter(email=email_param)
+        return queryset
