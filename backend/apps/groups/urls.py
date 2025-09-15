32d67b3c1873c3ce48d4325abbb9840fbab878a4
Marc Caramoan <mcar4177@uni.sydney.edu.au>
2025-09-15 14:35:15 +1000

    #73 - added support for POST method to insert a new Coutries row with tests

--- backend/apps/groups/urls.py
+++ backend/apps/groups/urls.py
@@ -1,3 +1,7 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import CountryViewSet
 
-urlpatterns = []
+router = DefaultRouter()
+router.register(r'countries', CountryViewSet, basename="countries")
+urlpatterns = router.urls
--- backend/apps/groups/views.py
+++ backend/apps/groups/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAdminUser, IsAuthenticated, AllowAny
+from .models import Groups, Countries
+from .serializers import CountrySerializer
 
 # Create your views here.
+
+class CountryViewSet(viewsets.ModelViewSet):
+  queryset = Countries.objects.all()
+  serializer_class = CountrySerializer
+  
+  def get_permissions(self):
+    # allow read for anybody and only write for admin
+    if self.action in ["list", "retrieve"]:
+      return [AllowAny()]
+    return [IsAdminUser()] # to check if the user has .is_staff flag
