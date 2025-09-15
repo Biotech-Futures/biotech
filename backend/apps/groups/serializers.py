b824749cfa692e0cc0476c6fba171e35c92259b1
Tyler Signo <tsit2615@uni.sydney.edu.au>
2025-09-15 15:56:33 +1000

    created group members serializers

--- backend/apps/groups/serializers.py
+++ backend/apps/groups/serializers.py
@@ -1,7 +1,12 @@
 from rest_framework import serializers
-from .models import Countries
+from .models import Countries, GroupMembers
 
 class CountrySerializer(serializers.ModelSerializer):
   class Meta:
     model = Countries
     fields = ['id', 'country_name']
+
+class GroupMemberSerializer(serializers.ModelSerializer):
+  class Meta:
+    model = GroupMembers
+    fields = ['id', 'group', 'user']
