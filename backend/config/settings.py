49ff1d108c2f5830db8af6b75f7fd5622b1517e4
edberd suwandi <esuw5153@uni.sydney.edu.au>
2025-09-18 11:50:57 +1000

    #91 enhanced: fixed merge conflicts patch v3

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -116,17 +116,22 @@ WSGI_APPLICATION = "config.wsgi.application"
 #         "NAME": BASE_DIR / "db.sqlite3",
 #     }
 # }
-
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": "biotech_db",
-        "USER": "postgres",
-        "PASSWORD": "zackzzzzzo3z",
-        "HOST": "localhost",
+        "NAME": "appdb",
+        "USER": "kosin",
+        "PASSWORD": "t3$t1NG_postgresql_DB!",
+        "HOST": 'azure-postgresql-db-trial.postgres.database.azure.com',
         "PORT": "5432",
+        "OPTIONS": {"sslmode": "require",
+                    "connect_timeout": 5,
+                    },
+            "CONN_MAX_AGE": 0
+
     }
 }
+
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 
