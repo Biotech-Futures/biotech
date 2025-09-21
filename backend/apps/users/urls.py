767a40d3d3fc32c6a74c8af269c1432a0b519fa0
Minh <qngu0097@uni.sydney.edu.au>
2025-09-21 18:54:42 +1000

    #37 register new user

--- backend/apps/users/urls.py
+++ backend/apps/users/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from . import views
-from .views import UsersRetrieveView, UserListHTMLView, UsersRetrieveUpdateView, MeRetrieveView
+from .views import UsersRetrieveView, UserListHTMLView, UsersRetrieveUpdateView, MeRetrieveView, UserRegisterView
 
 urlpatterns = [
     path("api/v1/users/<int:pk>/", UsersRetrieveUpdateView.as_view(), name="user-detail"),
     path("api/v1/users/", UserListHTMLView.as_view(), name="UserListHTMLView"),
     path("api/v1/users/me", MeRetrieveView.as_view(), name="MeListHTMLView"),
+    path('api/v1/wow', UserRegisterView.as_view(), name = "wow")
 ]
