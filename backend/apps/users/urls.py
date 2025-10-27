0fed686896071e6f05354271ada1f50d678a1a45
edbert suwandi <esuw5153@uni.sydney.edu.au>
2025-10-27 11:47:01 +1100

    hotfix: no trailing slah for api/v1/users/me/

--- backend/apps/users/urls.py
+++ backend/apps/users/urls.py
@@ -5,7 +5,7 @@ from .views import UsersRetrieveView, UserListHTMLView, UsersRetrieveUpdateView,
 urlpatterns = [
     path("users/<int:pk>/", UsersRetrieveUpdateView.as_view(), name="user-detail"),
     path("users/", UserListHTMLView.as_view(), name="UserListHTMLView"),
-    path("users/me", MeRetrieveView.as_view(), name="MeListHTMLView"),
+    path("users/me/", MeRetrieveView.as_view(), name="MeListHTMLView"),
     path('registration', UserRegisterView.as_view(), name = "registration"),
     path('updjoinperms', ReceiveJoinPermissionView.as_view(), name = "join_perm"),
     path("groups/unallocated/students/", UnallocatedStudentsListView.as_view(), name="unallocated-students"),
