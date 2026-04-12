87060ed15cbd4bbd8785b037754ebf1f92f244ed
Lewis Zhang <yzha0931@uni.sydney.edu.au>
2026-04-12 21:17:24 +1000

    Delete admin/apps/web/src/routes/_auth/event.tsx

--- admin/apps/web/src/routes/_auth/event.tsx
+++
@@ -1,9 +0,0 @@
-import { createFileRoute } from "@tanstack/react-router";
-
-export const Route = createFileRoute("/_auth/event")({
-  component: RouteComponent,
-});
-
-function RouteComponent() {
-  return <div>Hello "/_auth/event"!</div>;
-}
