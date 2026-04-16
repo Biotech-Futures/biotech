6191cb3bff0636fbb7d6a6ca9354fa36bcb33a3d
Kai <kaipham.work@gmail.com>
2026-04-16 18:44:37 +1000

    Update: Delete Task Serializer & View

--- backend/apps/tasks/serializers.py
+++ backend/apps/tasks/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from .models import Tasks, Milestone
 
+
 class TaskSerializer(serializers.ModelSerializer):
     class Meta:
         model = Tasks
@@ -15,3 +16,9 @@ class TaskCreateSerializer(serializers.ModelSerializer):
     class Meta:
         model = Tasks
         fields = ["task_name", "due_date", "milestone", "task_description"]
+
+
+class DeleteTaskResponseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tasks
+        fields = ["id", "task_name", "due_date", "deleted_flag", "milestone", "task_description"]
