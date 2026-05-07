abb0b0e0515138a4694c5c7fd615d2cab7e81319
Lewis Zhang <yzha0931@uni.sydney.edu.au>
2026-05-07 22:00:24 +1000

    feat: add locationLink field to event form and view

--- adminweb/src/type/event.ts
+++ adminweb/src/type/event.ts
@@ -5,6 +5,7 @@ export type Event = {
   startDatetime: string;
   endsDatetime: string;
   location: string | null;
+  locationLink: string | null;
   deletedFlag: boolean;
   deletedDatetime: string | null;
   "eventImage(img)": string | null;
